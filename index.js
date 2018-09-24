56066d98e51b55e9874fec99f5689f3d5401dcff
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-24 15:55:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function inc(n){ n++; return n}
 function dec(n){ n-- ;return n}
 function makeInt(n){ return parseInt(n,10);}
 function preserveDecimal(n){ return parseFloat(n);}
+function log (){console.log(parseInt(0*399))}
+log();
 
