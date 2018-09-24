aa1e59880fcb906fd432f856252eca3afa467d22
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-24 15:55:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function multiply(a,b){return a*b}
 function divide(a,b){return a/b}
 function inc(n){ n++; return n}
 function dec(n){ n-- ;return n}
-function makeInt(n){ return parseInt(n,10);}
+function makeInt(n){ return parseInt(n);}
 function preserveDecimal(n){ return parseFloat(n);}
-function log (){console.log(parseInt(0*399))}
-log();
 
